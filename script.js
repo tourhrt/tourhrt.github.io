13d9c71c958ce2b8f47d0e14bdc6d57ab28fb9a9
tourhrt <tourhrt@gmail.com>
2025-02-17 19:34:33 +0700

    birthday project

---
+++ script.js
@@ -0,0 +1,26 @@
+// trigger to play music in the background with sweetalert
+window.addEventListener('load', () => {
+    Swal.fire({
+        title: 'Do you want to play music in the background?',
+        // text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+        cancelButtonText: 'No',
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.querySelector('.song').play();
+            animationTimeline();
+        } else {
+            animationTimeline();
+        }
+    });
+});
+
+function showTime() {
+    document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+}
+showTime();
+setInterval(showTime, 1000);
